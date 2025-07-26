00b7d3586967ee6f8cde337a611af271e8601216
Abdi Gashahun <abdigashahun0@gmail.com>
2025-07-26 11:33:31 +0300

    Finished now u can learn how to do auth with Better Auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -10,64 +10,69 @@ import { nextCookies } from "better-auth/next-js";
 import { organization } from "better-auth/plugins";
 import { Resend } from "resend";
 import { ac, admin, member, owner } from "./auth/permissions";
-
 const resend = new Resend(process.env.RESEND_API_KEY as string);
-
 export const auth = betterAuth({
-    emailVerification: {
-        sendVerificationEmail: async ({ user, url }) => {
-            resend.emails.send({
-                from: `${process.env.EMAIL_SENDER_NAME} <${process.env.EMAIL_SENDER_ADDRESS}>`,
-                to: user.email,
-                subject: "Verify your email",
-                react: VerifyEmail({ username: user.name, verifyUrl: url }),
-            });
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url }) => {
+      resend.emails.send({
+        from: `${process.env.EMAIL_SENDER_NAME} <${process.env.EMAIL_SENDER_ADDRESS}>`,
+        to: user.email,
+        subject: "Verify your email",
+        react: VerifyEmail({ username: user.name, verifyUrl: url }),
+      });
+    },
+    sendOnSignUp: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  emailAndPassword: {
+    enabled: true,
+    sendResetPassword: async ({ user, url }) => {
+      resend.emails.send({
+        from: `${process.env.EMAIL_SENDER_NAME} <${process.env.EMAIL_SENDER_ADDRESS}>`,
+        to: user.email,
+        subject: "Reset your password",
+        react: ForgotPasswordEmail({
+          username: user.name,
+          resetUrl: url,
+          userEmail: user.email,
+        }),
+      });
+    },
+    requireEmailVerification: true,
+  },
+  databaseHooks: {
+    session: {
+      create: {
+        before: async (session) => {
+          const organization = await getActiveOrganization(session.userId);
+          return {
+            data: {
+              ...session,
+              activeOrganizationId: organization?.id,
+            },
+          };
         },
-        sendOnSignUp: true,
+      },
     },
-    socialProviders: {
-        google: {
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        },
-    },
-    emailAndPassword: {
-        enabled: true,
-        sendResetPassword: async ({ user, url }) => {
-            resend.emails.send({
-                from: `${process.env.EMAIL_SENDER_NAME} <${process.env.EMAIL_SENDER_ADDRESS}>`,
-                to: user.email,
-                subject: "Reset your password",
-                react: ForgotPasswordEmail({ username: user.name, resetUrl: url, userEmail: user.email }),
-            });
-        },
-        requireEmailVerification: true
-    },
-    databaseHooks: {
-        session: {
-            create: {
-                before: async (session) => {
-                    const organization = await getActiveOrganization(session.userId)
-                    return {
-                        data: {
-                            ...session,
-                            activeOrganizationId: organization?.id
-                        }
-                    }
-                }
-            }
-        }
-    },
-    database: drizzleAdapter(db, {
-        provider: "pg",
-        schema,
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+  plugins: [
+    organization({
+      ac,
+      roles: {
+        owner,
+        admin,
+        member,
+      },
     }),
-    plugins: [organization({
-        ac,
-        roles: {
-            owner,
-            admin,
-            member
-        }
-    }), nextCookies()]
+    nextCookies(),
+  ],
 });
